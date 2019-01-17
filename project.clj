294551176ca3229432c1fa15648b513c78b16ddd
Paul Stadig <paul@stadig.name>
2019-01-16 21:54:56 -0500

    Make clojars deploy repository.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/pjstadig/ring-cider-debug/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles
   {:dev {:dependencies [[compojure "1.6.1"]
